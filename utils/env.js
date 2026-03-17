e76a9b6c8e0659d5cc2d3786765df063cf2ef97c
hutiewei <37854474+hutiewei@users.noreply.github.com>
2026-03-17 07:52:04 +0000

    add log

--- utils/env.js
+++ utils/env.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+console.log('Loading environment variables...');
+console.log('COOKIE:', process.env.COOKIE);
+console.log('USER:', process.env.USER);
+
 module.exports = {
-  COOKIE: process.env.COOKIE
+  COOKIE: process.env.COOKIE,
+  user: process.env.USER
 };

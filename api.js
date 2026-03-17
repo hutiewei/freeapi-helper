fcf89f5834152538776acfb07dbe5b3b16504670
hutiewei <37854474+hutiewei@users.noreply.github.com>
2026-03-17 05:58:35 +0000

    url

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://freeapi.dgbmc.top';
 
 // 获取用户信息（用于演示登录有效性）
 async function userInfo(cookie) {
-  return axios.get(`${BASE_URL}/console/api/user/info`, {
+  return axios.get(`${BASE_URL}/console/api/personal`, {
     headers: {
       'cookie': cookie,
       'referer': 'https://freeapi.dgbmc.top/console'
@@ -14,7 +14,7 @@ async function userInfo(cookie) {
 // 签到接口
 async function signIn(cookie) {
   // 实际接口根据抓包可能需要调整URL和请求方式
-  return axios.post(`${BASE_URL}/console/api/user/signin`, null, {
+  return axios.post(`${BASE_URL}/console/api/user/checkin`, null, {
     headers: {
       'cookie': cookie,
       'referer': 'https://freeapi.dgbmc.top/console',
